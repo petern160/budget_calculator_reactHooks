c4b035782af793ee4d6994eb90ed567da16e5e41
petern160 <peternguyen160@gmail.com>
2019-08-26 18:30:19 -0700

    expenses now save to local storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import ExpenseList from "./components/ExpenseList";
 import Alert from "./components/Alert";
 import ExpenseForm from "./components/ExpenseForm";
 import uuid from "uuid/v4";
 
-const initialExpenses = [
-  { id: uuid(), charge: "rent", amount: 1600 },
-  { id: uuid(), charge: "car payment", amount: 400 },
-  { id: uuid(), charge: "credit card bill", amount: 1200 }
-];
+// const initialExpenses = [
+//   { id: uuid(), charge: "rent", amount: 1600 },
+//   { id: uuid(), charge: "car payment", amount: 400 },
+//   { id: uuid(), charge: "credit card bill", amount: 1200 }
+// ];
 
+const initialExpenses = localStorage.getItem("expenses")
+  ? JSON.parse(localStorage.getItem("expenses"))
+  : [];
 function App() {
   // **** state values ***** //
   // all expenses, add expense
@@ -29,6 +32,12 @@ function App() {
   const [edit, setEdit] = useState(false);
   // edit item
   const [id, setId] = useState(0);
+
+  // ****  useEffect ***** //
+  useEffect(() => {
+    console.log("we called useEffect");
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  });
 
   // **** functionality values ***** //
 
